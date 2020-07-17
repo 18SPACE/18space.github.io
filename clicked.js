3ada1e0bce77061a0910c20e14bf0ebb1719f6de
18SPACE <68350879+18SPACE@users.noreply.github.com>
2020-07-17 09:52:03 -0700

    Update clicked.js

--- clicked.js
+++ clicked.js
@@ -1,3 +1,14 @@
-function click(){  
- document.getElementById("main").innerHTML = "AUSSPOC"; 
+function click(var title){  
+ x = document.getElementById("title");
+ if(title == "ausspoc"){x.innerHTML = "AUSSPOC";}
+ else if(title == "canspoc"){x.innerHTML = "CANSpOC";}
+ else if(title == "cosmos"){x.innerHTML = "COSMOS";}
+ else if (title == "cspoc"){x.innerHTML = "CSpOC";}
+ else if (title == "gssac"){x.innerHTML = "GSSAC";}
+  else if (title == "isoc"){x.innerHTML = "ISoC";}
+  else if (title == "jaxa"){x.innerHTML = "JAXA";}
+ else if (title == "kasi"){x.innerHTML = "KASI";}
+ else if (title == "s3toc"){x.innerHTML = "S3TOC";}
+ #else if (title == " "){}
+ else{x.innerHTML = "Sentinel Participants Active Communications Enterprise";}
 }  
